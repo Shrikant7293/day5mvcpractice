dff6d9cc71af7f6454eec3d1cbe2d82e42d8214c
ADMIN$ <ADMIN$@DESKTOP-9TIC5VQ>
2022-11-14 20:39:08 +0530

    Add project files.

---
+++ day5mvcpractice/Controllers/BrightController.cs
@@ -0,0 +1,51 @@
+﻿using day5mvcpractice.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace day5mvcpractice.Controllers
+{
+	public class BrightController : Controller
+	{
+		public static List<Bright> Brights = new List<Bright>();
+
+		public IActionResult Student()
+		{
+			return View(Brights);
+		}
+
+		[HttpGet]
+		public IActionResult Add()
+		{
+			Bright Brights= new Bright();	
+			return View(Brights);
+		}
+
+		[HttpPost]
+		public IActionResult Add(Bright bright)
+		{
+			Brights.Add(bright);
+			return RedirectToAction("Student");
+		}
+
+
+		public IActionResult Delete(int id)
+		{
+			var Bright = Brights.FirstOrDefault(bright => bright.Id == id);
+			Brights.Remove(Bright);
+			return RedirectToAction("Student");
+		}
+
+		[HttpGet]
+
+		public IActionResult Edit()
+		{
+			return View();
+		}
+
+		[HttpPost]
+
+		public IActionResult Edit(Bright bright)
+		{
+			return View();
+		}
+	}
+}
---
+++ day5mvcpractice/Models/Bright.cs
@@ -0,0 +1,12 @@
+﻿namespace day5mvcpractice.Models
+{
+	public class Bright
+	{ 
+
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public string Course { get; set; }
+	}
+}
